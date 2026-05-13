83b67aa40c851b3752ea00bafaae784552b2707d
anando11112 <anando11112@gmail.com>
2026-05-13 23:14:19 +0600

    Update album.js

--- scripts/cmds/album.js
+++ scripts/cmds/album.js
@@ -51,7 +51,7 @@ module.exports = {
 
         onStart: async function ({ api, event, args, message, getLang }) {
                 const authorName = String.fromCharCode(77, 97, 104, 77, 85, 68);
-                if (this.config.author !== authorName) return api.sendMessage("You are not authorized to change the author name.", event.threadID, event.messageID);
+                if (module.exports.config.author !== authorName) return api.sendMessage("You are not authorized to change the author name.", event.threadID, event.messageID);
 
                 try {
                         const apiBase = await mahmud();
